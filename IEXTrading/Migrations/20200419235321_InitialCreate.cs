bbf5456a4aec285ae452bc3ca79a12a93ac3d9cc
Saumitra Korde <korde@mail.usf.edu>
2020-04-23 17:15:02 -0400

    Add project files.

---
+++ IEXTrading/Migrations/20200419235321_InitialCreate.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace IEXTrading.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    hospital_name = table.Column<string>(nullable: false),
+                    address = table.Column<string>(nullable: true),
+                    city = table.Column<string>(nullable: true),
+                    phone_number = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.hospital_name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equities",
+                columns: table => new
+                {
+                    EquityId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    change = table.Column<float>(nullable: false),
+                    changeOverTime = table.Column<float>(nullable: false),
+                    changePercent = table.Column<float>(nullable: false),
+                    close = table.Column<float>(nullable: false),
+                    date = table.Column<string>(nullable: true),
+                    high = table.Column<float>(nullable: false),
+                    label = table.Column<string>(nullable: true),
+                    low = table.Column<float>(nullable: false),
+                    open = table.Column<float>(nullable: false),
+                    symbol = table.Column<string>(nullable: true),
+                    unadjustedVolume = table.Column<int>(nullable: false),
+                    volume = table.Column<int>(nullable: false),
+                    vwap = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equities", x => x.EquityId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+
+            migrationBuilder.DropTable(
+                name: "Equities");
+        }
+    }
+}
---
+++ IEXTrading/Models/Company.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+namespace IEXTrading.Models
+{
+    public class Company
+    {
+        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string hospital_name { get; set; }
+        public string address { get; set; }
+        public string phone_number { get; set; }
+        public string hospital_overall_rating { get; set; }
+       
+    }
+}
